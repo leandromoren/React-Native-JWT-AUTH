856d4ea624864c730c6fafa4c2c955496fdffe2b
leandromoren <leandro.moren@outlook.com>
2023-12-03 02:34:03 -0300

    UI Login

--- App.tsx
+++ App.tsx
@@ -1,9 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { AuthProvider, useAuth } from './app/context/AuthContext';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './app/screens/Home';
+import Login from './app/screens/Login';
+import React from 'react';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <>
-    </>
+    <AuthProvider>
+      <Layout></Layout>
+    </AuthProvider>
   );
+}
+
+export const Layout = () => {
+  const { authState, onLogout } =  useAuth();
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        {authState?.isAuthenticated ? (
+          <Stack.Screen name="Home" component={Home} options={{ headerRight: () => <Button onPress={onLogout} title="Sign Out" /> }} />
+        ) : (
+          <Stack.Screen name="Login" component={Login} />
+        )}
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
--- app/screens/Login.tsx
+++ app/screens/Login.tsx
@@ -1,12 +1,73 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+import { View, TextInput, Image, Button, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { useAuth } from '../context/AuthContext';
 
 const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const { onLogin, onRegister } = useAuth();
+
+    const login = async () => {
+        const result = await onLogin(email, password);
+        if (result && result.error) {
+            alert(result.msg);
+        }
+    }
+
+    const register = async () => {
+        const result = await onRegister(email, password);
+        if (result && result.error) {
+            alert(result.msg);
+        } else {
+            login();
+        }
+    }
+
     return (
-        <View>
-            <Text>Login</Text>
+        <View style={styles.container}>
+            <Image source={{ uri: 'https://galaxies.dev/img/logos/logo--blue.png' }} style={styles.image} />
+            <View>
+                <TextInput
+                    placeholder="Email"
+                    value={email}
+                    onChangeText={setEmail}
+                    style={styles.input}
+                />
+                <TextInput
+                    placeholder="Password"
+                    value={password}
+                    onChangeText={setPassword}
+                    style={styles.input}
+                    secureTextEntry
+                />
+                <Button title="Login" onPress={login} />
+                <Button title="Register" onPress={register} />
+            </View>
         </View>
     )
-}
+};
+
+const styles = StyleSheet.create({
+    container: {
+        alignItems: 'center',
+        width: '100%'
+    },
+    input: {
+        height: 44,
+        borderWidth: 1,
+        borderRadius: 4,
+        padding: 10,
+        backgroundColor: '#fff'
+    },
+    form: {
+        gap: 10,
+        width: '60%'
+    },
+    image: {
+        width: '50%',
+        height: '50%',
+        resizeMode: 'contain'
+    }
+})
 
 export default Login;
